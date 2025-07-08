af96eda1cc579b4e128b53cbc21730ea5eb59ece
arminas-bartkus <arminas.bartkus@gmail.com>
2025-07-08 20:18:42 +0100

    Create main JS logic minus listeners

--- script.js
+++ script.js
@@ -43,46 +43,49 @@ const gameController = {
         let currentGameBoardRow = currentGameBoard[locationY];
         let currentGameBoardCell = currentGameBoardRow[locationX];
 
-        if (currentGameBoardCell != "") {console.log("Erro!")}
+        if (currentGameBoardCell != "") {return true;}
             // THIS IS IF TRYING TO OVERRIDE
     },
-
     checkForWin: function(currentGameBoard) {
         
         console.log(currentGameBoard);
         let [[a, b, c], [d, e, f], [g, h, i]] = currentGameBoard;
 
-
-
-
-
-
-        const isFullCriteria = (gridSquare) => {return gridSquare !== ""}
+        
         
         let brokenDownBoard = [];
         currentGameBoard.forEach((item) => item.forEach((element) => {brokenDownBoard.push(element)}));
-
+        const isFullCriteria = (gridSquare) => {return gridSquare !== ""}
         brokenDownBoard.every(isFullCriteria); 
     
         if  ((a === b && b === c && (b === "X" || b === "O")) || (d === e && e === f && (e === "X" || e === "O")) 
         || (g === h && h === i && (h === "X" || h === "O")) || (a === d && d === g && (d === "X" || d === "O")) 
         || (b === e && e === h && (e === "X" || e === "O")) || (c === f && f === i && (f === "X" || f === "O")) 
         || (a === e && e === i && (e === "X" || e === "O")) || (c === e && e === g) && (e === "X" || e === "O")) {
-            console.log("win")
-            this.declareWinner();
+
+            let winner = ""
+            if (data.playerOne.isMyTurn === true) {
+                winner = data.playerOne.playerName;
+                data.playerOne.playerScore++;
+            }
+            else {
+                winner = data.playerTwo.playerName;
+                data.playerOne.playerScore++;
+            }
+
+            this.declareWinner(winner);
         }
 
-        // else
-         if (brokenDownBoard.every(isFullCriteria)) {
+        else if (brokenDownBoard.every(isFullCriteria)) {
+            
             console.log("draw");
+            // show model with reset point
         }
 
         else {
+
+            // continue game
             this.swapPlayerTurns();
-            console.log("continue game")
-            
-            //  console.log(data.playerOne.isMyTurn) 
-            // console.log(data.playerTwo.isMyTurn) 
         }
 
     },
@@ -99,46 +102,46 @@ const gameController = {
         }
     },
     
-    declareWinner: function() {
+    declareWinner: function(winner) {
+
+        console.log("The winner is " + winner.firstName + " " + winner.lastName)
+        data
     },
 
-createMark: function(row, column) {
-  
-    this.checkForOverrideAttempts(row, column)
+    createMark: function(row, column) {
+    
+        let sign;
+    
+        if(this.checkForOverrideAttempts(row, column)) {}
 
-    let sign;
+        else {
 
-    if (data.playerOne.isMyTurn === true) {
-        sign = "X";
-    }
-    else {sign = "O"}
+      
 
+            if (data.playerOne.isMyTurn === true) {
+                sign = "X";
+            }
+            else {
+                sign = "O";
+            }
 
+            //Place x or o at location
 
-        const currentGameBoard = data.gameBoard.gameBoard;
-
-        const foundRow = currentGameBoard[row];
-        foundRow[column] = sign;
+            const currentGameBoard = data.gameBoard.gameBoard;
+            const foundRow = currentGameBoard[row];
+            foundRow[column] = sign;
 
         this.checkForWin(currentGameBoard);
+
+        }
     },
 }
 
 gameController.startGame();
-gameController.createMark(1,1);
-gameController.createMark(1,2);
-gameController.createMark(0,0);
-gameController.createMark(1,0);
 
-
-
-gameController.createMark(0,1);
-
-gameController.createMark(2,0);
-
-gameController.createMark(0,2);
-
-gameController.createMark(2,1);
-
-gameController.createMark(2,2);
-
+gameController.createMark(0,0)
+gameController.createMark(2,0)
+gameController.createMark(1,1)
+gameController.createMark(2,1)
+gameController.createMark(0,2)
+gameController.createMark(2,2)
